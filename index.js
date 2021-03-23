652986a874aea30d66feaca577d4c649d85499b2
fahimahammed <fahimahammed.cse@gmail.com>
2021-03-23 23:32:06 +0600

    dynamically call api by id

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 
+const users = ['Asad', 'Faruk', 'Fahim', 'firoz', 'Shanto', 'Anika'];
+
 app.get('/', (req, res) => {
     const fruit = {
         product: 'Ada',
@@ -16,6 +18,17 @@ app.get('/fruits/banana', (req, res) => {
         quantitiy: 10000,
         price: 10000000
     });
+})
+
+
+app.get('/user/:id', (req, res) => {
+    const id = req.params.id;
+
+    // http://localhost:3000/user/1?sort=asc
+    console.log(req.query.sort);
+    
+    const name = users[id];
+    res.send({id, name});
 })
 
 app.listen(3000, () => console.log('Listening to port 3000'));
