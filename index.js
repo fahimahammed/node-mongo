46fb63f332af82656feafe3d47b620ae19f1297b
fahimahammed <fahimahammed.cse@gmail.com>
2021-03-23 23:08:16 +0600

    banana api

--- index.js
+++ index.js
@@ -3,7 +3,19 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send("Express Js")
+    const fruit = {
+        product: 'Ada',
+        price: 220
+    }
+    res.send(fruit)
 });
+
+app.get('/fruits/banana', (req, res) => {
+    res.send({
+        fruit: 'banana',
+        quantitiy: 10000,
+        price: 10000000
+    });
+})
 
 app.listen(3000, () => console.log('Listening to port 3000'));
